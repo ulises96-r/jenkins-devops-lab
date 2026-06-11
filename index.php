31e6cdc9c85fbfc8fd796da64fa0143c47018552
ulises <acedillo@ahorra.io>
2026-06-10 18:27:40 -0600

    Agregar pagina php

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+echo "<h1>Laboratorio Jenkins + PHP</h1>";
+
+echo "<hr>";
+
+echo "<strong>Version PHP:</strong> ";
+echo phpversion();
+
+echo "<br><br>";
+
+echo "<strong>Hostname:</strong> ";
+echo gethostname();
+
+echo "<br><br>";
+
+echo "<strong>Fecha:</strong> ";
+echo date('Y-m-d H:i:s');
+
+?>
