70e0011cd926639894cbf9abc29283cdf6d394fa
ulises <acedillo@ahorra.io>
2026-06-11 18:04:18 -0600

    Version 1.0

--- index.php
+++ index.php
@@ -4,17 +4,22 @@ echo "<h1>Laboratorio Jenkins + PHP</h1>";
 
 echo "<hr>";
 
-echo "<strong>Version PHP:</strong> ";
+echo "<h3>Version Aplicacion:</h3>";
+echo "v1.0";
+
+echo "<br><br>";
+
+echo "<h3>Version PHP:</h3>";
 echo phpversion();
 
 echo "<br><br>";
 
-echo "<strong>Hostname:</strong> ";
+echo "<h3>Hostname:</h3>";
 echo gethostname();
 
 echo "<br><br>";
 
-echo "<strong>Fecha:</strong> ";
+echo "<h3>Fecha:</h3>";
 echo date('Y-m-d H:i:s');
 
 ?>
